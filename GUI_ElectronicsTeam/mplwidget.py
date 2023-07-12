02e7ecc84298827b50bc578ded61b6ecedf392fd
NicoCellectric <127844980+NicoCellectric@users.noreply.github.com>
2023-07-12 13:31:01 +0200

    Added Plots for Current and Voltage
    S: Plots for the Electronics-Team GUI are added. Scaling and zero-levels are taken into account.
    
    S: Added a line for the maximum temperature
    
    S: Added min/max level update for the voltage value in the GUI

--- GUI_ElectronicsTeam/mplwidget.py
+++ GUI_ElectronicsTeam/mplwidget.py
@@ -7,6 +7,8 @@ from matplotlib.backends.backend_qt5agg import FigureCanvas
 
 from matplotlib.figure import Figure
 
+import matplotlib as plt
+
     
 class MplWidget(QWidget):
     
@@ -19,5 +21,14 @@ class MplWidget(QWidget):
         vertical_layout = QVBoxLayout()
         vertical_layout.addWidget(self.canvas)
         
-        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.canvas.axes1 = self.canvas.figure.add_subplot(311)
+        self.canvas.axes1.set_title("Voltage")
+        self.canvas.axes1.set_ylabel("V")
+        self.canvas.axes2 = self.canvas.figure.add_subplot(312)
+        self.canvas.axes2.set_title("Current")
+        self.canvas.axes2.set_ylabel("A")
+        self.canvas.axes3 = self.canvas.figure.add_subplot(313)
+        self.canvas.axes2.set_title("Temperature")
+        self.canvas.axes1.set_ylabel("°C")
+
         self.setLayout(vertical_layout)
