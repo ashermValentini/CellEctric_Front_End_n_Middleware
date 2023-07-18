542e2e77710894876c50bb2f528bc250a0f0105d
Asher <121964501+CellEctricLab@users.noreply.github.com>
2023-07-18 15:49:10 +0200

    temperture plotting on a proper thread now

--- GUI_BioTeam/functionality.py
+++ GUI_BioTeam/functionality.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Communication_Functions.communication_functions import *
 
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal
+from PyQt5.QtCore import Qt, QObject, QThread, pyqtSignal, pyqtSlot, QMutex
 from layout import Ui_MainWindow
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -19,22 +19,35 @@ PELTIER_OFF = "wCS-0"
 
 class TempWorker(QObject):
     update_temp = pyqtSignal(float)
-    interval = 100
+    interval = 10
 
     def __init__(self, device_serials):
         super(TempWorker, self).__init__()
         self.device_serials = device_serials
-        self.isRunning = True
+        self._is_running = False
+        self._lock = QMutex()
 
+    @pyqtSlot()
     def run(self):
-        while self.isRunning:
+        self._is_running = True
+        while True:
+            self._lock.lock()
+            if not self._is_running:
+                self._lock.unlock()
+                break
+            self._lock.unlock()
+            
             temperature = read_temperature(self.device_serials[3])
             if temperature is not None:
                 self.update_temp.emit(temperature)
-            time.sleep(self.interval/1000)  # converting interval to seconds for time.sleep
+            QThread.msleep(self.interval)
 
+    @pyqtSlot()
     def stop(self):
-        self.isRunning = False
+        self._lock.lock()
+        self._is_running = False
+        self._lock.unlock()
+
 
 
 class Functionality(QtWidgets.QMainWindow):
@@ -59,14 +72,16 @@ class Functionality(QtWidgets.QMainWindow):
        
         
     # Temp plotting (with threads)
-
-
+        
+        self.tempWorker = TempWorker(self.device_serials)
+        self.tempThread = QThread()
+        self.tempWorker.moveToThread(self.tempThread) 
+        self.tempWorker.update_temp.connect(self.update_temp_plot)
+        self.tempThread.started.connect(self.tempWorker.run)
         self.temp_is_plotting = False
 
         self.ui.temp_button.pressed.connect(self.start_stop_temp_plotting)
-        #self.ui.temp_button.pressed.connect(self.start_temp_plotting)
-        #self.timer = None
-                
+                    
         self.xdata = np.linspace(0, 499, 500)  
         self.plotdata = np.zeros(500)
 
@@ -83,7 +98,7 @@ class Functionality(QtWidgets.QMainWindow):
         self.plotdata = np.zeros(500)
         self.zerodata = [2000, 2000]
         
-        self.voltage_plot_interval = 30  # ms
+        self.voltage_plot_interval = 500  # ms
         self.maxval_pulse = 10  
         self.minval_pulse = -10
 
@@ -110,92 +125,10 @@ class Functionality(QtWidgets.QMainWindow):
 #region : PLOTTING FUNCTIONS  
 
     #region: Temperature Plot 
-    '''
-    def start_temp_plotting(self):
-        if not self.temp_is_plotting:   #if temp_is_plotting is false (ie the button has just been pressed to start plotting) the change the color to blue
-            # Change button color to blue
-            self.ui.temp_button.setStyleSheet("""
-                QPushButton {
-                    border: 2px solid white;
-                    border-radius: 10px;
-                    background-color: #0796FF;
-                    color: #FFFFFF;
-                    font-family: Archivo;
-                    font-size: 15px;
-                }
-
-                QPushButton:hover {
-                    background-color: rgba(7, 150, 255, 0.7);  /* 70% opacity */
-                }
-            """)
-            self.temp_is_plotting = True  #Change the status of temp_is_plotting from false to true because we are about to begin plotting
-            if self.timer is None:        #If the time is none we can be sure that we were in a state of not plotting but now we should go into a state of plotting 
-                self.timer = self.start_timer() #we get into a stat of plotting by starting the timer for the temp plot (which has been poorly named timer-too generic must change it)
-        else: #Else if temp_is_plotting is true then it means the button was pressed during a state of plotting and the user would like to stop plotting which means we need to:
-            # Change button color back to original
-            self.ui.temp_button.setStyleSheet("""
-                QPushButton {
-                    border: 2px solid white;
-                    border-radius: 10px;
-                    background-color: #222222;
-                    color: #FFFFFF;
-                    font-family: Archivo;
-                    font-size: 15px;
-                }
-
-                QPushButton:hover {
-                    background-color: rgba(7, 150, 255, 0.7);  /* 70% opacity */
-                }
-
-                QPushButton:pressed {
-                    background-color: #0796FF;
-                }
-            """)
-            self.temp_is_plotting = False #Change the status of temp_is_plotting from true to False because we are about to stop plotting
-            if self.timer:                #If the temp plot timer is true it means we were indeed in a state of plotting and can therefore be sure that we need to stop plotting
-                self.timer.stop()         #to stop plottng simply stop the timer
-                self.timer = None         #but remember to set the timer to none so that we can start the timer the next time we click the button
-                self.ui.axes_voltage.clear()
-                self.ui.canvas_voltage.draw()
-
-                
-    def start_timer(self):
-        timer = QtCore.QTimer()
-        timer.setInterval(self.interval)
-        timer.timeout.connect(self.update_temp_plot)
-        timer.start()
-        return timer
-
-    def update_temp_plot(self):
-        temperature = read_temperature(self.device_serials[3])
-        if temperature is not None:
-            shift = 1
-            self.plotdata = np.roll(self.plotdata, -shift)
-            self.plotdata[-shift:] = temperature
-            self.ydata = self.plotdata[:]
-            
-            self.xdata = np.roll(self.xdata, -shift)
-            self.xdata[-1] = self.xdata[-2] + 1  # This will keep increasing the count on the x-axis
-
-            self.ui.axes_voltage.clear()
-            self.ui.axes_voltage.plot(self.xdata, self.ydata, color='#0796FF')
-            self.ui.axes_voltage.yaxis.set_major_formatter(ticker.FormatStrFormatter('%0.1f'))
-            self.ui.axes_voltage.set_ylim(min(self.ydata) - 1, max(self.ydata) + 10)  # dynamically update y range
-
-            self.ui.axes_voltage.set_xlabel('Time (ms)', color='#FFFFFF')
-            self.ui.axes_voltage.set_ylabel('Temperature (°C)', color='#FFFFFF')
-            self.ui.axes_voltage.set_title('My Title', color='#FFFFFF')
-        
-        self.ui.canvas_voltage.draw()
-        '''
     def start_stop_temp_plotting(self):
         if not self.temp_is_plotting:  # If not currently plotting, start
-            self.tempThread = QThread()
-            self.tempWorker = TempWorker(self.device_serials)
-            self.tempWorker.moveToThread(self.tempThread)
-            self.tempWorker.update_temp.connect(self.update_temp_plot)
-            self.tempThread.started.connect(self.tempWorker.run)
-            self.tempThread.start()
+            self.tempThread.start()  # Start the existing thread
+
             self.temp_is_plotting = True
             self.ui.temp_button.setStyleSheet("""
                 QPushButton {
@@ -212,9 +145,10 @@ class Functionality(QtWidgets.QMainWindow):
                 }
             """)
         else:  # If currently plotting, stop
-            self.tempWorker.stop()
-            self.tempThread.quit()
-            self.tempThread.wait()
+            self.tempWorker.stop()  # Ask the worker to stop
+            self.tempThread.quit()  # Ask the thread to stop
+            self.tempThread.wait()  # Wait for the thread to stop
+            
             self.temp_is_plotting = False
             self.ui.temp_button.setStyleSheet("""
                 QPushButton {
@@ -234,6 +168,7 @@ class Functionality(QtWidgets.QMainWindow):
                     background-color: #0796FF;
                 }
             """)
+    
     def update_temp_plot(self, temperature):
         shift = 1
         self.plotdata = np.roll(self.plotdata, -shift)
@@ -361,18 +296,7 @@ class Functionality(QtWidgets.QMainWindow):
             """)
             self.sucrose_is_pumping = True  
             
-            flowRate1 = self.ui.line_edit_sucrose.text()
-            if flowRate1: 
-                flowRate1 = float(flowRate1)
-            else: 
-                flowRate1=0.00
-    
-            flowRate2 = self.ui.line_edit_ethanol.text()
-            if flowRate2: 
-                flowRate2 = float(flowRate2)
-            else: 
-                flowRate2=0.00
-                
+
             print("MESSAGE: " + VALVE_ON)
             self.device_serials[2].write(VALVE_ON.encode())
             msg = self.device_serials[2].readline()
